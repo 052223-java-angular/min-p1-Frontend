5bc7dba1396db54283b09d2d871851bd3c64a389
minhe7735 <61667570+minhe7735@users.noreply.github.com>
2023-06-20 22:48:17 -0400

    can modify comments. need refactor

--- src/app/user/component/comment-card/comment-card.component.ts
+++ src/app/user/component/comment-card/comment-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PostService } from '../../service/post.service';
+import { ModifyCommentPayload } from 'src/app/models/ModifyCommentPayload';
 
 @Component({
   selector: 'app-comment-card',
@@ -7,4 +10,44 @@ import { Component, Input } from '@angular/core';
 })
 export class CommentCardComponent {
   @Input() comment: any;
+  modifyCommentC: boolean = false;
+
+  formGroup: FormGroup;
+  constructor(private fb: FormBuilder, private postService: PostService) {
+    this.formGroup = fb.group({
+      comment: ['', [Validators.required]],
+    });
+
+  }
+
+  modifyComment() {
+    if (this.formGroup.invalid) {
+      return;
+    }
+
+    const payload: ModifyCommentPayload = {
+      comment: this.formGroup.controls['comment'].value,
+      userId: sessionStorage.getItem('id') || '',
+      commentId: this.comment.id
+    };
+
+    this.postService.modifyComment(payload).subscribe({
+      next: comment => {
+        console.log("success");
+        // Handle the sucsess response
+        // TODO: Add code for handling success response
+      },
+      error: error => {
+        console.log("failed");
+        // Handle the error response
+        // TODO: Add code for handling error response
+      }
+    })
+
+    this.modifyCommentC = false;
+  }
+
+  modifyCommentClick() {
+    this.modifyCommentC = true;
+  }
 }
