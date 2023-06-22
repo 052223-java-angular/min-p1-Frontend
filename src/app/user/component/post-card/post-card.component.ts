7582c6f8e09ab94448ff6303a8d99a8df27409c7
minhe7735 <61667570+minhe7735@users.noreply.github.com>
2023-06-21 21:28:04 -0400

    some voting refactor

--- src/app/user/component/post-card/post-card.component.ts
+++ src/app/user/component/post-card/post-card.component.ts
@@ -10,8 +10,15 @@ import { Router } from '@angular/router';
 })
 export class PostCardComponent {
   @Input() post!: Post;
+  up: number = 0;
+  down: number = 0;
   constructor(private router: Router) { };
   postClick() {
     this.router.navigate([`/post/${this.post.id}`])
+  }
+
+  ngOnInit() {
+    this.up = this.post.votes.filter((vote: { [x: string]: any; }) => vote['vote']).length;
+    this.down = this.post.votes.length - this.up;
   }
 }
