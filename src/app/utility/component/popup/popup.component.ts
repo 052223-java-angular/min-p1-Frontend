8062920554fced4e65958b996f92292f31bbf1c8
minhe7735 <61667570+minhe7735@users.noreply.github.com>
2023-06-25 07:03:00 -0400

    popup setup

--- src/app/utility/component/popup/popup.component.ts
+++ src/app/utility/component/popup/popup.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { LoginComponent } from 'src/app/user/component/login/login.component';
 
 @Component({
   selector: 'app-popup',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./popup.component.css']
 })
 export class PopupComponent {
-
+  error: string = "";
+  constructor(public dialogRef: MatDialogRef<any>, @Inject(MAT_DIALOG_DATA) public data: any) {
+    if (data) {
+      this.error = data;
+    }
+  }
 }
